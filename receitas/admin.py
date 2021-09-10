5934d2e52c6ea4edef408a4c74897e58d8c5f7af
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-10 17:25:16 -0300

    Ajustando o Django admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,3 +3,8 @@ from .models import Receita
 @admin.register(Receita)
 class ReceitaAdmin(admin.ModelAdmin):
    list_display =  ('nome_receita', 'ingredientes', 'modo_preparo', 'tempo_preparo', 'rendimento', 'categoria', 'date_receita', )
+   list_display_links = ('nome_receita', 'ingredientes', ) #Linka para o abrir o editar
+   #filtros
+   search_fields = ('nome_receita',) #Buscar, por escrito
+   list_filter = ('categoria',) #Mostra apenas os dados da categoria escolhida
+   list_per_page = 2 #Limita o tanto de itens exibidos por tela
