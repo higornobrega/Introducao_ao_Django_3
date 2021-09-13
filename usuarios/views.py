45ec7473a313229babe7ae03133e522bbcef7609
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-13 17:20:12 -0300

    Usuário criado

---
+++ usuarios/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def cadastro(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def dashbord(request):
+    pass    
+    
