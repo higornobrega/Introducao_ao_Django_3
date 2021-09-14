119ab9c340fe08e73e7597fa0d686a55f1494088
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-14 11:21:04 -0300

    Formulário no Django

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import redirect, render
-
+from django.contrib.auth.models import User
 def cadastro(request):
     if request.method == 'POST':
-        print('PAssou')
-        redirect('login')
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        print(nome, email, senha, senha2)
+        if not nome.strip():
+            print("O componente não pode ficar em Branco")
+            return redirect('cadastro')
+        if not email.strip():
+            print("O campo nome não pode ficar em Branco")
+            return redirect('cadastro')   
+        if senha != senha2:
+            print("Senha diferente da confimação")
+            return redirect('cadastro')     
+        if User.objects.filter(email=email).exists():
+            print("Usuário já cadastrado")
+            return redirect('cadastro')   
+
+        user = User.objects.create_user(username=nome, email=email, password=senha) 
+        user.save()
+        print('Usuario cadastrado') 
+        return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
 
