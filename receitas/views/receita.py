8c3ac176f5a06eedadd592eab1be6d6a222a8ad5
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-22 22:21:34 -0300

    retirando busca

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -19,16 +19,6 @@ def receita(request, receita_id):
     }
     return render(request,'receitas/receita.html', receita_a_exibir)
 
-def busca(request):
-    lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True) 
-    if 'buscar' in request.GET:
-        nome_a_buscar = request.GET['buscar']
-        lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
-
-    dados ={
-        'receitas' : lista_receitas
-    }
-    return render(request, 'receitas/buscar.html', dados)
 
 
 def cria_receita(request):
