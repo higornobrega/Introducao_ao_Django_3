b421683b976561855f090b1b28258424189b24c8
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-30 08:40:45 -0300

    final

---
+++ receitas/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<int:receita_id>', receita, name='receita'),
+    path('buscar', busca, name='buscar'),
+    path('cria/receita', cria_receita, name='cria_receita'),
+    path('deleta/<int:receita_id>', deleta_receita, name='deleta_receita'),
+    path('editar/<int:receita_id>', editar_receita, name='editar_receita'),
+    path('atualiza_receita', atualiza_receita, name='atualiza_receita'),
+]
